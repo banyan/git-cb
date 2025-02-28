0c2e1b5355f6f3141a5f1ed467f6ccd9508ab758
Kohei Hasegawa <ameutau@gmail.com>
2025-03-01 06:19:43 +0900

    Update sha256

--- Formula/git-cb.rb
+++ Formula/git-cb.rb
@@ -2,7 +2,7 @@ class GitCb < Formula
   desc "Git subcommand to create a branch based on commit message"
   homepage "https://github.com/banyan/git-cb"
   url "https://github.com/banyan/git-cb/archive/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256_AFTER_RELEASE"
+  sha256 "c457b5280a871778699c95438b7f55f719cdd2cc0ec9fa98a5350f360aec6c38"
   license "MIT"
 
   def install
