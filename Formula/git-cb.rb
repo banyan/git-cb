b26c484d3752b930bcd8c1bdcb7e0b26aa17016f
GitHub Action <action@github.com>
2025-02-28 21:45:49 +0000

    Update formula for v0.1.2

--- Formula/git-cb.rb
+++ Formula/git-cb.rb
@@ -1,8 +1,8 @@
 class GitCb < Formula
   desc "Git subcommand to create a branch based on commit message"
   homepage "https://github.com/banyan/git-cb"
-  url "https://github.com/banyan/git-cb/archive/v0.1.0.tar.gz"
-  sha256 "c457b5280a871778699c95438b7f55f719cdd2cc0ec9fa98a5350f360aec6c38"
+  url "https://github.com/banyan/git-cb/archive/v0.1.2.tar.gz"
+  sha256 "f28a26b0da255284ea6ffe0f013aecf47a4a45213aed36fdf5b83a2e856be987"
   license "MIT"
 
   def install
